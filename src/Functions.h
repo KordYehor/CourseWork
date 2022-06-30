43719be83f017400eb8e62874408cf8fbd6c7d90
KordYehor <96293422+KordYehor@users.noreply.github.com>
2022-06-30 09:40:51 +0300

    Update Functions.h

--- src/Functions.h
+++ src/Functions.h
@@ -6,4 +6,4 @@ std::string stringRightAlign(int newlength, std::string str = " ", char char_ = 
 std::string stringLeftAlign(int newlength, std::string str = " ", char char_ = ' ');
 std::string stringCenterAlign(int newlength, std::string str = " ", char char_ = ' ');
 void instruction();
-int GetNum(int returnBig = -1);
+int GetNum(int returnNum = -1);
