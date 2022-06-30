4b0933094f62ddc9215a4e472ba62fc654eeb2b4
KordYehor <96293422+KordYehor@users.noreply.github.com>
2022-06-30 09:39:18 +0300

    Delete Functions.h

--- src/Functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-
-std::string stringRightAlign(int newlength, std::string str = " ", char char_ = ' ');
-std::string stringLeftAlign(int newlength, std::string str = " ", char char_ = ' ');
-std::string stringCenterAlign(int newlength, std::string str = " ", char char_ = ' ');
-void instruction();
-int GetNum();
